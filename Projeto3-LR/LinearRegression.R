44d123bb40a1ed6dcc3d3599cadc33bafb138fad
jmbds <joaomiguelbsilva@hotmail.com>
2019-12-20 16:40:04 +0000

    cenas

--- Projeto3-LR/LinearRegression.R
+++ Projeto3-LR/LinearRegression.R
@@ -1,1 +1,11 @@
-data = read.table(file="all_results.in", header=TRUE);
+data = read.table(file="p_results.in", header=TRUE);
+bubble = data[data$ALGORITHM == 'bubble',];
+quick = data[data$ALGORITHM == 'quick',];
+merge = data[data$ALGORITHM == 'merge',];
+insertion = data[data$ALGORITHM == 'insertion',];
+
+plot(merge$ERRORS ~ merge$PROBABILITY)
+lr.out = lm(merge$ERRORS ~ merge$PROBABILITY);
+summary(lr.out)
+abline(lr.out, col="RED")
+
